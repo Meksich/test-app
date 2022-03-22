80055c7fe846755ef7650339618cf4b985620309
Meksich <maks10031003@gmail.com>
2022-03-22 18:52:36 +0200

    Add header and main part of web application

--- test-project/Frontend/src/app/app.module.ts
+++ test-project/Frontend/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMainComponent } from './app-main/app-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
